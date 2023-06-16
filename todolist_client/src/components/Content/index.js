eb06da4171727e66513020bbac00b970aecc2ebf
Amanda Nobre <amandapnobre@gmail.com>
2023-06-16 17:37:01 -0300

    call a request on ToDoList and show the initial page when list is empty

--- todolist_client/src/components/Content/index.js
+++ todolist_client/src/components/Content/index.js
@@ -1,35 +1,60 @@
-import React from "react";
-import './index.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./index.css";
 import { CreateToDoButton } from "../CreateToDoButton";
-// import { WelcomeText } from "../WelcomeText";
+import { WelcomeText } from "../WelcomeText";
 import { ToDoTask } from "../ToDoTask";
 import { ToDoTitle } from "../ToDoTitle";
 import { ToDoList } from "../ToDoList";
 import { AddTaskButton } from "../AddTaskButton";
 
-export function Content(){
-    return(
-        <div className="content">
-            <div className="button-div">
-                <CreateToDoButton />
-                {/* for each to do list */}
-                <ToDoList />
-                <ToDoList />
-            </div>
-            {/* if listas criadas == 0*/}
-            {/* <div className="content-todo-div">
-                <WelcomeText />
-            </div> */}
-            {/* if listas criadas > 0*/}
-            <div className="content-todo-div">
-                <ToDoTitle />
-                {/* for each to do list */}
-                <ToDoTask />
-                <ToDoTask />
-                <ToDoTask />
-                <AddTaskButton />
-            </div>
+export function Content() {
+  const [toDoLists, setToDoLists] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/to_do_lists/")
+      .then((response) => {
+        setToDoLists(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao obter a lista de to-do lists:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {toDoLists.length > 0 ? (
+          <div className="content">
+          <div className="button-div">
+            <CreateToDoButton />
+            {toDoLists.map((toDoList) => (
+              <ToDoList title={toDoList.title} id={toDoList.id} />
+            ))}
+          </div>
+          <div className="content-todo-div">
+            <ToDoTitle />
+            {/* for each to do list */}
+            <ToDoTask />
+            <ToDoTask />
+            <ToDoTask />
+            <AddTaskButton />
+          </div>
         </div>
-    )
+        ) : (
+          <div className="content">
+        <div className="button-div">
+          <CreateToDoButton />
+          {toDoLists.map((toDoList) => (
+            <ToDoList title={toDoList.title} id={toDoList.id} />
+          ))}
+        </div>
+        <div className="content-todo-div">
+                <WelcomeText />
+            </div>
+        </div>
+        )}
+    </>
+  );
 }
--- todolist_client/src/components/ToDoList/index.js
+++ todolist_client/src/components/ToDoList/index.js
@@ -1,19 +1,27 @@
 import React from "react";
-import './index.css'
+import './index.css';
 
-export function ToDoList(){
-    return(
-        <div className="todolist-div">
-            <div className="todolist-text-div">
-                <p className="todolist-text">To Do List Title</p>
-            </div>
-            {/* <div className="todolist-three-dots">
+export function ToDoList({ title, id }) {
+  const handleDelete = () => {
+    // Lógica de exclusão da lista
+  };
+
+  return (
+    <div className="todolist-div">
+      <div className="todolist-text-div">
+        <p className="todolist-text">{title}</p>
+      </div>
+
+    {/* <div className="todolist-three-dots">
                     FAZER O DROPDOWN
                     <img src="assets/images/three-dots.png" className="todolist-three-dots-image" alt="three-dots"/>
             </div> */}
-            <div className="task-x">
-                <img className="task-x-image" src="assets/images/x.png"  alt="x"/>
-            </div>
-        </div>
-    )
+
+      <div className="task-x">
+        <button className="task-x-button" onClick={handleDelete}>
+          <img className="task-x-image" src="assets/images/x.png" alt="x" />
+        </button>
+      </div>
+    </div>
+  );
 }
