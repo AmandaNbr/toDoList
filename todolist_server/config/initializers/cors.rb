336a853ba494c22500dee4c4762d193092a66265
Amanda Nobre <amandapnobre@gmail.com>
2023-06-17 05:05:19 -0300

    setting up docker-compose and dockerfiles

--- todolist_server/config/initializers/cors.rb
+++ todolist_server/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
+    origins "*"
     resource "/to_do_lists/*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
